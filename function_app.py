d3b919f11559b25ac4189ad1aab6cea29ad81dab
EduardoCorripioB <156282077+EduardoCorripioB@users.noreply.github.com>
2024-02-28 10:55:02 -0800

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -1,28 +1,6 @@
-import bs4
-import requests
-import pyodbc
-from datetime import *
-import time
+#Import function
+from app import cryptoscrapper
 
-
-##############################################################################################################
-#CreatelistFunction
-def createlist(bsvar):
-    list = []
-    for n in bsvar:
-        list.append(n.text)
-    return list
-
-def testSQL():
-    databaseconnection = pyodbc.connect('''Driver={ODBC Driver 18 for SQL Server};
-        Server=tcp:cryptodataproject.database.windows.net,1433;
-        Database=cryptodata;Uid=account1;Pwd=Contrasena1;
-        Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30''')
-    return databaseconnection
-
-timer = 5
-
-##############################################################################################################
 #Time Trigger
 import logging
 import azure.functions as func
@@ -33,51 +11,7 @@ app = func.FunctionApp()
               use_monitor=False) 
 def timer_trigger(myTimer: func.TimerRequest) -> None:
     ###########################################################################################################################
-    #Getpagedata
-    paginabase = 'https://www.coindesk.com/data/'
-    requestwebpage = requests.get(paginabase)
-    bswebpage = bs4.BeautifulSoup(requestwebpage.text, 'lxml')
-    
-    #Constant directions
-    coinandabradd = '.inner-column'
-    coinpriceadd = '.typography__StyledTypography-sc-owin6q-0.lnOdBs'
-    coindayperc = '.percentage'
-    
-    #Getdata
-    #Coinnames
-    coindatabs = bswebpage.select(coinandabradd)
-    coinnamelist = createlist(coindatabs)
-    #Coinprices
-    coinpricebs = bswebpage.select(coinpriceadd)
-    coinpricelist = createlist(coinpricebs)
-    #Coinpercentage
-    coinpercbs = bswebpage.select(coindayperc)
-    coinperclist = createlist(coinpercbs)
-    
-    todaydate = datetime.today().strftime('%Y-%m-%dT%H:%M:%S')
-    
-    #######################################################
-    #SQL Database conection
-    j = 1
-    while j == 1:
-        try:
-            testSQL()
-        except: 
-            print('Error en conexion, intentando nuevamente')
-            time.sleep(timer)
-        else:
-            j = 2
-            print('Proceso Exitoso')
-            databaseconnection = testSQL()
-            database = databaseconnection.cursor()
-            i = 0
-            while i<len(coinnamelist):
-                database.execute(f'''INSERT INTO dbo.Crypto(Coin,Price,Date, Percentage) 
-                VALUES 
-                ('{coinnamelist[i]}','{coinpricelist[i][1:]}','{todaydate}','{coinperclist[i]}')''')
-                i += 1
-            database.commit()
-    
+    cryptoscrapper()
     ###########################################################################################################################
     logging.info('Python timer trigger function executed.')
 
